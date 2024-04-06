b3185af7e6bf2bfbd3bf548e8dfd1d7a219dd59a
elmerhernandezz <elmeralbertoperez@gmail.com>
2024-04-06 09:55:45 -0600

    Se agregan validaciones para el modelo de Estudiante

--- ADSProject/Controllers/EstudiantesController.cs
+++ ADSProject/Controllers/EstudiantesController.cs
@@ -23,6 +23,10 @@ namespace ADSProject.Controllers
         {
             try
             {
+                if(!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.estudiante.AgregarEstudiante(estudiante);
 
                 if (contador > 0)
@@ -50,6 +54,10 @@ namespace ADSProject.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.estudiante.ActualizarEstudiante(idEstudiante, estudiante);
                 if (contador > 0)
                 {
