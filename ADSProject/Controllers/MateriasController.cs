3fee0fb9c9cb3c9f1eeae42056d15a4fb5714ddb
elmerhernandezz <elmeralbertoperez@gmail.com>
2024-04-11 15:43:19 -0600

    Se agregan validaciones para el modelo Materia

--- ADSProject/Controllers/MateriasController.cs
+++ ADSProject/Controllers/MateriasController.cs
@@ -23,6 +23,10 @@ namespace ADSProject.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.materia.AgregarMateria(materia);
 
                 if (contador > 0)
@@ -50,6 +54,10 @@ namespace ADSProject.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.materia.ActualizarMateria(idMateria, materia);
                 if (contador > 0)
                 {
--- ADSProject/obj/Debug/net8.0/ADSProject.AssemblyInfo.cs
+++ ADSProject/obj/Debug/net8.0/ADSProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADSProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb1f81e61d6b85ac9c7eeeb21c80d8c2a84ca65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd3f7e21aac1af3b0198ec5781abbac5463833b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADSProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADSProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
